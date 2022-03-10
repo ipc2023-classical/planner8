c21722482ef84e6bf38adf987f1b8c24401f47e6
Jendrik Seipp <jendrik.seipp@liu.se>
2022-03-10 14:54:32 +0100

    [novelty] Add experiment.

---
+++ experiments/novelty/2022-03-10-B-novelty-functions.py
@@ -0,0 +1,98 @@
+#! /usr/bin/env python
+
+import os
+import shutil
+
+import project
+
+
+REPO = project.get_repo_base()
+BENCHMARKS_DIR = os.environ["DOWNWARD_BENCHMARKS"]
+SCP_LOGIN = "seipp@login-infai.scicore.unibas.ch"
+REMOTE_REPOS_DIR = "/infai/seipp/projects"
+if project.REMOTE:
+    SUITE = project.SUITE_SATISFICING
+    ENV = project.BaselSlurmEnvironment(email="jendrik.seipp@liu.se")
+else:
+    SUITE = ["depot:p01.pddl", "grid:prob01.pddl", "gripper:prob01.pddl"]
+    ENV = project.LocalEnvironment(processes=2)
+
+CONFIGS = [
+    (f"ff-novelty-{aggregate}", [
+        "--evaluator", "hff=ff(transform=adapt_costs(one))",
+        "--search", f"""lazy_greedy([hff,novelty(width=2, aggregate={aggregate})],preferred=[hff],
+                                cost_type=one,reopen_closed=false)"""])
+    for aggregate in ["min", "max", "sum"]
+]
+BUILD_OPTIONS = []
+DRIVER_OPTIONS = ["--overall-time-limit", "5m"]
+REVS = [
+    ("novelty", ""),
+]
+ATTRIBUTES = [
+    "error",
+    "run_dir",
+    "search_start_time",
+    "search_start_memory",
+    "total_time",
+    "h_values",
+    "coverage",
+    "expansions",
+    "memory",
+    project.EVALUATIONS_PER_TIME,
+]
+
+exp = project.FastDownwardExperiment(environment=ENV)
+for config_nick, config in CONFIGS:
+    for rev, rev_nick in REVS:
+        algo_name = f"{rev_nick}:{config_nick}" if rev_nick else config_nick
+        exp.add_algorithm(
+            algo_name,
+            REPO,
+            rev,
+            config,
+            build_options=BUILD_OPTIONS,
+            driver_options=DRIVER_OPTIONS,
+        )
+exp.add_suite(BENCHMARKS_DIR, SUITE)
+
+exp.add_parser(exp.EXITCODE_PARSER)
+exp.add_parser(exp.TRANSLATOR_PARSER)
+exp.add_parser(exp.SINGLE_SEARCH_PARSER)
+exp.add_parser(project.DIR / "parser.py")
+exp.add_parser(exp.PLANNER_PARSER)
+
+exp.add_step("build", exp.build)
+exp.add_step("start", exp.start_runs)
+exp.add_fetcher(name="fetch")
+
+if not project.REMOTE:
+    exp.add_step(
+        "remove-eval-dir", shutil.rmtree, exp.eval_dir, ignore_errors=True
+    )
+    project.add_scp_step(exp, SCP_LOGIN, REMOTE_REPOS_DIR)
+
+project.add_absolute_report(
+    exp, attributes=ATTRIBUTES, filter=[project.add_evaluations_per_time]
+)
+
+attributes = ["expansions"]
+pairs = [
+    ("novelty:01-cg", "novelty:02-ff"),
+]
+suffix = "-rel" if project.RELATIVE else ""
+for algo1, algo2 in pairs:
+    for attr in attributes:
+        exp.add_report(
+            project.ScatterPlotReport(
+                relative=project.RELATIVE,
+                get_category=None if project.TEX else lambda run1, run2: run1["domain"],
+                attributes=[attr],
+                filter_algorithm=[algo1, algo2],
+                filter=[project.add_evaluations_per_time],
+                format="tex" if project.TEX else "png",
+            ),
+            name=f"{exp.name}-{algo1}-vs-{algo2}-{attr}{suffix}",
+        )
+
+exp.run_steps()
