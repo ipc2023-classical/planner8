0f276843496b30e496890976de996fda49f5c7d5
Jendrik Seipp <jendrik.seipp@liu.se>
2022-05-08 10:13:41 +0200

    [novelty] Use standard novelty in NoveltyEvaluator.

--- src/search/novelty/novelty_evaluator.cc
+++ src/search/novelty/novelty_evaluator.cc
@@ -12,7 +12,6 @@ namespace novelty {
 NoveltyEvaluator::NoveltyEvaluator(const Options &opts)
     : Heuristic(opts),
       width(opts.get<int>("width")),
-      aggregation_function(opts.get<AggregationFunction>("aggregate")),
       debug(opts.get<utils::Verbosity>("verbosity") == utils::Verbosity::DEBUG) {
     utils::g_log << "Initializing novelty heuristic..." << endl;
 
@@ -25,50 +24,31 @@ NoveltyEvaluator::NoveltyEvaluator(const Options &opts)
     }
     utils::g_log << "Facts: " << num_facts << endl;
 
-    fact_novelty.resize(num_facts, 0);
+    seen_facts.resize(num_facts, false);
     if (width == 2) {
-        fact_pair_novelty.resize(num_facts);
+        seen_fact_pairs.resize(num_facts);
         // We could store only the "triangle" of values instead of the full square.
         for (int fact_id = 0; fact_id < num_facts; ++fact_id) {
-            fact_pair_novelty[fact_id].resize(num_facts, 0);
+            seen_fact_pairs[fact_id].resize(num_facts, false);
         }
     }
 }
 
-int NoveltyEvaluator::get_and_increase_fact_pair_novelty(int fact_id1, int fact_id2) {
+bool NoveltyEvaluator::get_and_set_fact_pair_seen(int fact_id1, int fact_id2) {
     if (fact_id1 > fact_id2) {
         swap(fact_id1, fact_id2);
     }
     assert(fact_id1 < fact_id2);
-    int novelty = fact_pair_novelty[fact_id1][fact_id2]++;
-    return novelty;
+    bool seen = seen_fact_pairs[fact_id1][fact_id2];
+    seen_fact_pairs[fact_id1][fact_id2] = true;
+    return seen;
 }
 
-static u_int64_t combine_values(uint64_t val1, uint64_t val2, AggregationFunction func) {
-    if (func == AggregationFunction::MIN) {
-        return min(val1, val2);
-    } else if (func == AggregationFunction::MAX) {
-        return max(val1, val2);
-    } else if (func == AggregationFunction::SUM) {
-        return val1 + val2;
-    } else {
-        ABORT("unknown aggregation function");
-    }
-}
-
-int NoveltyEvaluator::compute_and_increase_novelty(const State &state) {
+int NoveltyEvaluator::compute_and_set_novelty(const State &state) {
     int num_vars = state.size();
-    uint64_t novelty = (aggregation_function == AggregationFunction::MIN)
-        ? numeric_limits<uint64_t>::max()
-        : 0;
-    for (FactProxy fact_proxy : state) {
-        FactPair fact = fact_proxy.get_pair();
-        int fact_id = get_fact_id(fact);
-        novelty = combine_values(novelty, fact_novelty[fact_id]++, aggregation_function);
-    }
-    if (debug) {
-        cout << "width-1 novelty: " << novelty << endl;
-    }
+    int novelty = 3;
+
+    // Check for novelty 2.
     if (width == 2) {
         for (int var1 = 0; var1 < num_vars; ++var1) {
             FactPair fact1 = state[var1].get_pair();
@@ -76,26 +56,30 @@ int NoveltyEvaluator::compute_and_increase_novelty(const State &state) {
             for (int var2 = var1 + 1; var2 < num_vars; ++var2) {
                 FactPair fact2 = state[var2].get_pair();
                 int fact_id2 = get_fact_id(fact2);
-                novelty = combine_values(
-                    novelty,
-                    get_and_increase_fact_pair_novelty(fact_id1, fact_id2),
-                    aggregation_function);
-                if (debug) {
-                    cout << fact1 << " " << fact2 << endl;
-                    cout << "width-2 novelty: " << novelty << endl;
+                bool seen = get_and_set_fact_pair_seen(fact_id1, fact_id2);
+                if (!seen) {
+                    novelty = 2;
                 }
             }
         }
     }
-    if (novelty > static_cast<uint64_t>(numeric_limits<int>::max())) {
-        ABORT("novelty sum overflow");
+
+    // Check for novelty 1.
+    for (FactProxy fact_proxy : state) {
+        FactPair fact = fact_proxy.get_pair();
+        int fact_id = get_fact_id(fact);
+        if (!seen_facts[fact_id]) {
+            seen_facts[fact_id] = true;
+            novelty = 1;
+        }
     }
+
     return novelty;
 }
 
 int NoveltyEvaluator::compute_heuristic(const State &state) {
     // No need to convert ancestor state since we only allow cost transformations.
-    int novelty = compute_and_increase_novelty(state);
+    int novelty = compute_and_set_novelty(state);
     if (debug) {
         cout << novelty << endl;
     }
@@ -105,8 +89,6 @@ int NoveltyEvaluator::compute_heuristic(const State &state) {
 static shared_ptr<Heuristic> _parse(OptionParser &parser) {
     parser.add_option<int>(
         "width", "maximum conjunction size", "2", Bounds("1", "2"));
-    parser.add_enum_option<AggregationFunction>(
-        "aggregate", {"min", "max", "sum"}, "aggregation function", "min");
     Heuristic::add_options_to_parser(parser);
     utils::add_log_options_to_parser(parser);
     Options opts = parser.parse();
--- src/search/novelty/novelty_evaluator.h
+++ src/search/novelty/novelty_evaluator.h
@@ -4,27 +4,20 @@
 #include "../heuristic.h"
 
 namespace novelty {
-enum class AggregationFunction {
-    MIN,
-    MAX,
-    SUM,
-};
-
 class NoveltyEvaluator : public Heuristic {
     const int width;
-    const AggregationFunction aggregation_function;
     const bool debug;
 
     std::vector<int> fact_id_offsets;
-    std::vector<int> fact_novelty;
-    std::vector<std::vector<int>> fact_pair_novelty;
+    std::vector<int> seen_facts;
+    std::vector<std::vector<bool>> seen_fact_pairs;
 
     int get_fact_id(FactPair fact) const {
         return fact_id_offsets[fact.var] + fact.value;
     }
 
-    int get_and_increase_fact_pair_novelty(int fact_id1, int fact_id2);
-    int compute_and_increase_novelty(const State &state);
+    bool get_and_set_fact_pair_seen(int fact_id1, int fact_id2);
+    int compute_and_set_novelty(const State &state);
 
 protected:
     virtual int compute_heuristic(const State &ancestor_state) override;
