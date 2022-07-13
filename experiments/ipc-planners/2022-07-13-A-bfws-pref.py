304b1b98964fb4405e5a4b3b167c306d199d4a80
Augusto B. Corrêa <augusto.blaascorrea@unibas.ch>
2022-07-13 18:09:56 +0200

    [novelty] Add script for the experiment using BFWS-pref

---
+++ experiments/ipc-planners/2022-07-13-A-bfws-pref.py
@@ -0,0 +1,145 @@
+#! /usr/bin/env python
+
+"""
+Example experiment for running Singularity planner images.
+
+The time and memory limits set with Lab can be circumvented by solvers
+that fork child processes. Their resource usage is not checked. If you're
+running solvers that don't check their resource usage like Fast Downward,
+we recommend using cgroups or the "runsolver" tool to enforce resource
+limits. Since setting time limits for solvers with cgroups is difficult,
+the experiment below uses the "runsolver" tool, which has been used in
+multiple SAT competitions to enforce resource limits. For the experiment
+to run, the runsolver binary needs to be on the PATH. You can obtain a
+runsolver copy from https://github.com/jendrikseipp/runsolver.
+
+A note on running Singularity on clusters: reading large Singularity files
+over the network is not optimal, so we recommend copying the images to a
+local filesystem (e.g., /tmp/) before running experiments.
+"""
+
+import os
+from pathlib import Path
+import platform
+import sys
+
+import project
+
+from downward import suites
+from downward.reports.absolute import AbsoluteReport
+from lab.environments import BaselSlurmEnvironment, LocalEnvironment
+from lab.experiment import Experiment
+
+
+# Create custom report class with suitable info and error attributes.
+class BaseReport(AbsoluteReport):
+    INFO_ATTRIBUTES = []
+    ERROR_ATTRIBUTES = [
+        "domain",
+        "problem",
+        "algorithm",
+        "unexplained_errors",
+        "error",
+        "node",
+    ]
+
+
+NODE = platform.node()
+RUNNING_ON_CLUSTER = NODE.endswith((".scicore.unibas.ch", ".cluster.bc2.ch"))
+DIR = Path(__file__).resolve().parent
+REPO = DIR.parent
+IMAGES_DIR = Path("/infai/blaas/planning/planning-images")
+assert IMAGES_DIR.is_dir(), IMAGES_DIR
+BENCHMARKS_DIR = os.environ["AUTOSCALE_BENCHMARKS_SAT"]
+MEMORY_LIMIT = 3584
+if RUNNING_ON_CLUSTER:
+    SUITE = project.SUITE_AUTOSCALE_SAT
+    ENVIRONMENT = BaselSlurmEnvironment(
+        partition="infai_2",
+        email="my.name@unibas.ch",
+        memory_per_cpu="3872M",
+        export=["PATH"],
+        setup=BaselSlurmEnvironment.DEFAULT_SETUP)
+    TIME_LIMIT = 300
+else:
+    SUITE = ["gripper:prob01.pddl"]
+    ENVIRONMENT = LocalEnvironment(processes=2)
+    TIME_LIMIT = 5
+
+ATTRIBUTES = [
+    "cost",
+    "coverage",
+    "search_time",
+    "generated",
+    "expansions",
+    "peak_memory",
+    "error",
+    "run_dir",
+    "raw_memory",
+    "runtime",
+    "virtual_memory",
+]
+
+exp = Experiment(environment=ENVIRONMENT)
+exp.add_step("build", exp.build)
+exp.add_step("start", exp.start_runs)
+exp.add_fetcher(name="fetch")
+exp.add_parser(DIR / "singularity-parser.py")
+
+
+def get_image(name):
+    planner = name.replace("-", "_")
+    image = IMAGES_DIR / (name + ".img")
+    assert image.is_file(), image
+    return planner, image
+
+
+IMAGES = [get_image("ipc2018-agl-lapkt-bfws-pref")]
+
+for planner, image in IMAGES:
+    exp.add_resource(planner, image, symlink=True)
+
+exp.add_resource("run_singularity", DIR / "run-singularity.sh")
+exp.add_resource("filter_stderr", DIR / "filter-stderr.py")
+
+for planner, _ in IMAGES:
+    for task in suites.build_suite(BENCHMARKS_DIR, SUITE):
+        run = exp.add_run()
+        run.add_resource("domain", task.domain_file, "domain.pddl")
+        run.add_resource("problem", task.problem_file, "problem.pddl")
+        # Use runsolver to limit time and memory. It must be on the system
+        # PATH. Important: we cannot use time_limit and memory_limit of
+        # Lab's add_command() because setting the same memory limit with
+        # runsolver again using setrlimit fails.
+        run.add_command(
+            "run-planner",
+            [
+                "runsolver",
+                "-C",
+                TIME_LIMIT,
+                "-V",
+                MEMORY_LIMIT,
+                "-w",
+                "watch.log",
+                "-v",
+                "values.log",
+                "{run_singularity}",
+                f"{{{planner}}}",
+                "{domain}",
+                "{problem}",
+                "sas_plan",
+            ],
+        )
+        # Remove temporary files from old Fast Downward versions.
+        run.add_command("rm-tmp-files", ["rm", "-f", "output.sas", "output"])
+        run.add_command("filter-stderr", [sys.executable, "{filter_stderr}"])
+
+        run.set_property("domain", task.domain)
+        run.set_property("problem", task.problem)
+        run.set_property("algorithm", planner)
+        run.set_property("id", [planner, task.domain, task.problem])
+
+report = Path(exp.eval_dir) / f"{exp.name}.html"
+exp.add_report(BaseReport(attributes=ATTRIBUTES), outfile=report)
+
+exp.run_steps()
