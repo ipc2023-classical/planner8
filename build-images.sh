c481a6b20c8fd842ccf1958d9b343e5434b4f4d1
Augusto B. Corrêa <augusto.blaascorrea@unibas.ch>
2023-03-21 15:32:37 +0100

    Update image building script and gitignore

--- build-images.sh
+++ build-images.sh
@@ -1,13 +1,19 @@
 #! /usr/bin/env bash
 
 echo "Building image for Scorpion Novelty - Satisficing Track"
-apptainer build scorpion-novelty-sat.img Apptainer.sn_sat
+apptainer build scorpion-novelty-sat.sif Apptainer.sn_sat
 
 echo "Building image for Scorpion Novelty - Agile Track"
-apptainer build scorpion-novelty-agl.img Apptainer.sn_agl
+apptainer build scorpion-novelty-agl.sif Apptainer.sn_agl
+
+echo "Building image for Powerlifted - Satisficing Track"
+apptainer build powerlifted-sat.sif Apptainer.powerlifted_sat
+
+echo "Building image for Powerlifted - Agile Track"
+apptainer build powerlifted-agl.sif Apptainer.powerlifted_agl
 
 echo "Building image for Levitron - Satisficing Track"
-apptainer build levitron-sat.img Apptainer.levitron_sat
+apptainer build levitron-sat.sif Apptainer.levitron_sat
 
 echo "Building image for Levitron - Agile Track"
-apptainer build levitron-agl.img Apptainer.levitron_agl
+apptainer build levitron-agl.sif Apptainer.levitron_agl
