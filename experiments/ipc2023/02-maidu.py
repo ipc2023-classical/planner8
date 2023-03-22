ef42e68e926ec2173d4c22d8dde6b7ddfb0195f0
Jendrik Seipp <jendrik.seipp@liu.se>
2023-03-22 20:37:21 +0100

    Add logs for Maidu.

---
+++ experiments/ipc2023/02-maidu.py
@@ -0,0 +1,58 @@
+#! /usr/bin/env python
+
+import csv
+from pathlib import Path
+
+from lab.experiment import Experiment
+
+import project
+
+
+DIR = Path(__file__).resolve().parent
+REPO = project.get_repo_base()
+ATTRIBUTES = [
+    "error",
+    #"run_dir",
+    "coverage",
+    "search_time",
+    "total_time",
+]
+
+exp = Experiment()
+exp.add_step("remove-combined-properties", project.remove_properties, Path(exp.eval_dir))
+
+domains_with_axioms = set()
+domains_with_cond_effs = set()
+with open("domain_properties.csv") as f:
+    reader = csv.reader(f)
+    next(reader)  # Skip header
+    for row in reader:
+        domain, axioms, cond_effs = row
+        assert axioms in {"True", "False"}
+        assert cond_effs in {"True", "False"}
+        if axioms == "True":
+            domains_with_axioms.add(domain)
+        if cond_effs == "True":
+            domains_with_cond_effs.add(domain)
+print("Axioms:", sorted(domains_with_axioms))
+print("Conditional effects:", sorted(domains_with_cond_effs))
+print("Both:", sorted(domains_with_axioms & domains_with_cond_effs))
+
+for expname in [
+    #"powerlifted-htg",
+    #"powerlifted-patricks-benchmark",
+    "scorpion-novelty-htg",
+    "scorpion-novelty-patricks-benchmark",
+]:
+    project.fetch_algorithms(exp, expname, filters=[project.strip_properties])
+
+project.add_absolute_report(
+    exp, attributes=ATTRIBUTES, name=f"{exp.name}-full"
+)
+
+properties_hardest = Path(exp.eval_dir) / "properties-hardest.json"
+exp.add_report(project.Hardest30Report(), outfile=properties_hardest, name="keep-only-30-hardest-tasks")
+exp.add_step("compress-properties", project.compress, properties_hardest)
+#project.add_absolute_report(exp, attributes=ATTRIBUTES, name=f"{exp.name}-hardest")
+
+exp.run_steps()
--- learners/learn-portfolios.sh
+++ learners/learn-portfolios.sh
@@ -3,7 +3,10 @@
 set -euo pipefail
 
 POWERLIFTED_DATA=../experiments/ipc2023/data/01-powerlifted-eval/properties-hardest.json.xz
+MAIDU_DATA=../experiments/ipc2023/data/02-maidu-eval/properties-hardest.json.xz
 
-#./batch-stonesoup.sh ${POWERLIFTED_DATA} sat 1800 | tee batch-stonesoup-sat.txt
-./stonesoup.py --track sat ${POWERLIFTED_DATA} 40 | tee stonesoup-sat.txt
-#./greedy.py ${POWERLIFTED_DATA} --track sat | tee greedy-sat.txt
+#./batch-stonesoup.sh ${POWERLIFTED_DATA} sat 1800 | tee batch-stonesoup-powerlifted-sat.txt
+./stonesoup.py --track sat ${POWERLIFTED_DATA} 40 | tee stonesoup-powerlifted-sat.txt
+
+#./batch-stonesoup.sh ${MAIDU_DATA} sat 1800 | tee batch-stonesoup-maidu-sat.txt
+./stonesoup.py --track sat ${MAIDU_DATA} 40 | tee stonesoup-maidu-sat.txt
