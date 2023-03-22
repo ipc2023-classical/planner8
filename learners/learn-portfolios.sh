ce50ff3a9aaec8709b2e9d5bd9f1a550b4b1840c
Jendrik Seipp <jendrik.seipp@liu.se>
2023-03-22 21:20:43 +0100

    Don't try to create portfolio of lifted and ground planners, use individual portfolios instead.

--- learners/learn-portfolios.sh
+++ learners/learn-portfolios.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 POWERLIFTED_DATA=../experiments/ipc2023/data/01-powerlifted-eval/properties-hardest.json.xz
 MAIDU_DATA=../experiments/ipc2023/data/02-maidu-eval/properties-hardest.json.xz
-LEVITRON_DATA=../experiments/ipc2023/data/03-levitron-eval/properties-hardest.json.xz
+#LEVITRON_DATA=../experiments/ipc2023/data/03-levitron-eval/properties-hardest.json.xz
 
 #./batch-stonesoup.sh ${POWERLIFTED_DATA} sat 1800 | tee batch-stonesoup-powerlifted-sat.txt
 #./stonesoup.py --track sat ${POWERLIFTED_DATA} 40 | tee stonesoup-powerlifted-sat.txt
@@ -13,4 +13,4 @@ LEVITRON_DATA=../experiments/ipc2023/data/03-levitron-eval/properties-hardest.js
 #./stonesoup.py --track sat ${MAIDU_DATA} 40 | tee stonesoup-maidu-sat.txt
 
 #./batch-stonesoup.sh ${LEVITRON_DATA} sat 1800 | tee batch-stonesoup-levitron-sat.txt
-./stonesoup.py --track sat ${LEVITRON_DATA} 20 | tee stonesoup-levitron-sat.txt
+#./stonesoup.py --track sat ${LEVITRON_DATA} 20 | tee stonesoup-levitron-sat.txt
